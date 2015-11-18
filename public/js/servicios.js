3352b56b5c2275e961a65c5b8063dad79404e73d
vicbaporu <lopez.victor94@gmail.com>
2015-11-18 13:26:58 -0600

    Cambio sección de servicios
    Subo la sección de servicios modificada
    -En las primeras tres imagenes tiene efecto propuesta para esta sección
    -En la imagen de abajo tiene el efecto original hecho a mano, considero
    que se parece a la versión original
    -Subo también corrección para el login

--- public/js/servicios.js
+++ public/js/servicios.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() 
 {
 
-  $('#collapsibleUno').collapse();
-  
-
-  $('#detalleMaritimo').on("click",function()
+  //Codigo para transicion propuesta en detalle
+  //$('#collapsibleUno').collapse();
+  $('#detalleAduanaMaritimo').on("click",function()
   {
     if(!$('#collapsiblePanel').hasClass('alert-maritimo') )
     {
@@ -14,15 +13,29 @@ $( document ).ready(function()
           );
         if(!$("#collapsibleUno").hasClass('in'))
           $('#collapsibleUno').collapse('toggle');
+
+          var src = $("#imgTransportes").attr('src');
+
+          if(src == "/img/Servicios/maritimoFocus.jpg")
+            $("#imgTransportes").attr("src","/img/Servicios/Servicios-transporte.jpg");
+          else
+            $("#imgTransportes").attr("src","/img/Servicios/maritimoFocus.jpg");
+        
     }
     else
     {
         $('#collapsibleUno').collapse('toggle');
+
+        var src = $("#imgTransportes").attr('src');
+          if(src == "/img/Servicios/maritimoFocus.jpg")
+            $("#imgTransportes").attr("src","/img/Servicios/Servicios-transporte.jpg");
+          else
+            $("#imgTransportes").attr("src","/img/Servicios/maritimoFocus.jpg");
     }
 
   });
 
-  $('#detalleAereo').on('click',function()
+  $('#detalleAduanaAereo').on('click',function()
   {
     if(!$('#collapsiblePanel').hasClass('alert-aereo') )
     {
@@ -59,7 +72,7 @@ $( document ).ready(function()
 
   });
 
-  $('#detalleTerrestre').on('click',function()
+  $('#detalleAduanaTerrestre').on('click',function()
   {
     if(!$('#collapsiblePanel').hasClass('alert-terrestre') )
     {
@@ -70,15 +83,74 @@ $( document ).ready(function()
          
         if(!$("#collapsibleUno").hasClass('in'))
           $('#collapsibleUno').collapse('toggle');
+
+        var src = $("#imgTransportes").attr('src');
+          if(src == "/img/Servicios/terrestreFocus.jpg")
+            $("#imgTransportes").attr("src","/img/Servicios/Servicios-transporte.jpg");
+          else
+            $("#imgTransportes").attr("src","/img/Servicios/terrestreFocus.jpg");
     }
     else
     {
         $('#collapsibleUno').collapse('toggle');
+
+        var src = $("#imgTransportes").attr('src');
+          if(src == "/img/Servicios/terrestreFocus.jpg")
+            $("#imgTransportes").attr("src","/img/Servicios/Servicios-transporte.jpg");
+          else
+            $("#imgTransportes").attr("src","/img/Servicios/terrestreFocus.jpg");
     }
 
   });
 
+
+
+  //Codigo para mandar la entrada de la transición de detalle
+  $(function() 
+  {
+    // run the currently selected effect
+    function runEffect() 
+    {
+      // get effect type from
+      var selectedEffect = "drop";
+ 
+      // most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 0 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 200, height: 60 } };
+      }
+ 
+      // run the effect
+      $( "#detalleAduana" ).toggle( selectedEffect, options, 500 );
+    };
+ 
+    // set effect from select menu value
+    $( "#btnAduana" ).click(function() {
+      runEffect();
+    });
+    $( "#aduanaClose" ).click(function() {
+      $( "#btnAduana" ).click();
+    });
+  });
+  //Codigo para setear la posición de cuadro de detalle
+  var width = $("#imgTransportes").width();
+  var height = $("#imgTransportes").height();
+
+  $("#detalleAduana").css("display","none"); 
+  $("#detalleAduana").css("position","relative");
+  $("#detalleAduana").css("background-color","rgba(0,0,0,.8)");
+  $("#detalleAduana").css("color","white");
+  $("#detalleAduana").css("opacity","1");
+
+  $("#detalleAduana").css("bottom",height);
+  $("#detalleAduana").css("margin-bottom",-height);
+  $("#detalleAduana").css("left",width/3);
+
+  $("#detalleAduana").css("height",height);
+  $("#detalleAduana").css("width", (width/3)*2  );
+  $("#detalleAduana").css("marginleft"," 24em");
+
 });
-
-
-
