cb214364904be8c2b135483594b6ca05ca9ebab6
vicbaporu <lopez.victor94@gmail.com>
2015-11-19 08:57:24 -0600

    Prueba
    prueba

--- public/js/servicios.js
+++ public/js/servicios.js
@@ -72,36 +72,6 @@ $( document ).ready(function()
 
   });
 
-  $('#detalleAduanaTerrestre').on('click',function()
-  {
-    if(!$('#collapsiblePanel').hasClass('alert-terrestre') )
-    {
-        $('#collapsiblePanel').removeClass();
-        $('#collapsiblePanel').addClass(
-          " alert alert-terrestre"
-          );
-         
-        if(!$("#collapsibleUno").hasClass('in'))
-          $('#collapsibleUno').collapse('toggle');
-
-        var src = $("#imgTransportes").attr('src');
-          if(src == "/img/Servicios/terrestreFocus.jpg")
-            $("#imgTransportes").attr("src","/img/Servicios/Servicios-transporte.jpg");
-          else
-            $("#imgTransportes").attr("src","/img/Servicios/terrestreFocus.jpg");
-    }
-    else
-    {
-        $('#collapsibleUno').collapse('toggle');
-
-        var src = $("#imgTransportes").attr('src');
-          if(src == "/img/Servicios/terrestreFocus.jpg")
-            $("#imgTransportes").attr("src","/img/Servicios/Servicios-transporte.jpg");
-          else
-            $("#imgTransportes").attr("src","/img/Servicios/terrestreFocus.jpg");
-    }
-
-  });
 
 
 
