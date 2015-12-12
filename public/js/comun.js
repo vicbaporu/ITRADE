ef75d5fd8dc8558b1423aad6ef55d57f1d798db9
vicbaporu <lopez.victor94@gmail.com>
2015-12-11 23:42:07 -0600

    Cambios finales REV 4
    -Agrego boton de login para resolucion movil asi como cotizar en todas
    las resoluciones y botones de lenguaje para resolucion movil

--- public/js/comun.js
+++ public/js/comun.js
@@ -8,45 +8,45 @@ $( document ).ready(function()
 	var entroEng = false;
 	var entroEsp = false;
 
-	$("#imgEng").mouseover(function() 
+	$(".imgEng").mouseover(function() 
 	{
-		var src = $('#imgEng').attr('src');
+		var src = $('.imgEng').attr('src');
 		if(src == "/img/inglesH.png")
 		{
-  			$("#imgEng").attr("src","/img/ingles.png")
+  			$(".imgEng").attr("src","/img/ingles.png")
   			entroEng = true;
   		}
 	});
-	$("#imgEng").mouseleave(function() {
-  		var src = $('#imgEng').attr('src');
+	$(".imgEng").mouseleave(function() {
+  		var src = $('.imgEng').attr('src');
 		if(src == "/img/ingles.png" && entroEng == true)
-  			$("#imgEng").attr("src","/img/inglesH.png")
+  			$(".imgEng").attr("src","/img/inglesH.png")
 	});
 
-	$("#imgEsp").mouseover(function() 
+	$(".imgEsp").mouseover(function() 
 	{
-		var src = $('#imgEsp').attr('src');
+		var src = $('.imgEsp').attr('src');
 		if(src == "/img/españolH.png")
 		{
-  			$("#imgEsp").attr("src","/img/español.png")
+  			$(".imgEsp").attr("src","/img/español.png")
   			entroEsp = true;
   		}
 	});
-	$("#imgEsp").mouseleave(function() {
-  		var src = $('#imgEsp').attr('src');
+	$(".imgEsp").mouseleave(function() {
+  		var src = $('.imgEsp').attr('src');
 		if(src == "/img/español.png" && entroEsp == true)
-  			$("#imgEsp").attr("src","/img/españolH.png")
+  			$(".imgEsp").attr("src","/img/españolH.png")
 	});
 
     if(getLang("es"))
     {
-        $("#imgEsp").attr("src","/img/español.png");
-        $("#imgEng").attr("src","/img/inglesH.png");
+        $(".imgEsp").attr("src","/img/español.png");
+        $(".imgEng").attr("src","/img/inglesH.png");
     }
     else if(getLang("en"))
     {
-        $("#imgEsp").attr("src","/img/españolH.png");
-        $("#imgEng").attr("src","/img/ingles.png");
+        $(".imgEsp").attr("src","/img/españolH.png");
+        $(".imgEng").attr("src","/img/ingles.png");
     }
 
 
